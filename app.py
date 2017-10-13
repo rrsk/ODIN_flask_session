f7560b6285d21d7eadb286f7f1643932b7ed5f6c
Rishab Gupta <rishab0611@gmail.com>
2017-10-13 18:53:03 +0530

    Better

--- app.py
+++ app.py
@@ -10,3 +10,7 @@ def odin_intro():
 @app.route('/attendee/<name>')
 def member(name=None):
 	return render_template('attendee.html',name = name)
+
+
+# make your own routes below and send pull requests 
+# feel free to ask questions, we are there to help
